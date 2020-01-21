46edc4ec14f07322cb0402f0ae2e4c1b5185b44b
ruabelyan <58383424+ruabelyan@users.noreply.github.com>
2020-01-21 15:29:52 +0400

    React learn to json file

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostList from './script'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<PostList/>
+
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/script.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PostData from "../package.json"
+
+function PostList() {
+  return (
+    <div>
+        <h1> Hello </h1>
+        {/* {PostData.map((json,index)=>{
+            return <h1>{potDetail.name}
+        })}
+        */}
+        {/* <div className="user" id="userx">
+
+        </div>
+        <div className="user" id="usery">
+
+        </div>
+        <div className="user" id="userz">
+
+        </div>
+        <button className="btn">
+          render users
+        </button> */}
+    </div>
+    
+  );
+}
+
+export default PostList;
